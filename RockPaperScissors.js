6df8d0752704befa3ca17cd4c075c53364586509
Nathan Spain <219461355+nspain96@users.noreply.github.com>
2025-08-24 18:30:37 -0400

    Create first Function - Human Choice.

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,1 +1,12 @@
-console.log('Hiiiii')
+// This is an array that stores the results from the Rounds
+let TournamentResults = Array.from({ length: 5 });
+console.log(TournamentResults);
+
+function getHumanChoice(Choice){
+    // User Selects Their Choice
+    let HumanChoice = prompt("Please Enter Rock, Paper, or Scissors");
+    return(HumanChoice.toUpperCase());
+
+
+}
+
