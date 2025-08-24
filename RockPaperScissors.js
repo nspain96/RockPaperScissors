3bec99b77a323b3208c20d6777033638681f14a5
Nathan Spain <219461355+nspain96@users.noreply.github.com>
2025-08-24 18:48:10 -0400

    Add Function - Computer Choice

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -6,7 +6,19 @@ function getHumanChoice(Choice){
     // User Selects Their Choice
     let HumanChoice = prompt("Please Enter Rock, Paper, or Scissors");
     return(HumanChoice.toUpperCase());
-
-
 }
 
+function getComputerChoice(computer){
+    let RandNum = Math.random();
+    if (RandNum <= .333333){
+        return "ROCK";
+    } if (RandNum <= .6666666) {
+        return "PAPER";
+    } else {
+        return "SCISSORS"; 
+    }
+    }
+
+
+
+ 
