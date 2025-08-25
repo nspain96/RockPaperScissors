d653a6a7ed34d27547ced167cd655b1e0ee31d92
Nathan Spain <219461355+nspain96@users.noreply.github.com>
2025-08-24 21:15:26 -0400

    Add Play Round Function

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,14 +1,11 @@
-// This is an array that stores the results from the Rounds
-let TournamentResults = Array.from({ length: 5 });
-console.log(TournamentResults);
-
+// User Selects Their Choice
 function getHumanChoice(Choice){
-    // User Selects Their Choice
     let HumanChoice = prompt("Please Enter Rock, Paper, or Scissors");
     return(HumanChoice.toUpperCase());
 }
 
-function getComputerChoice(computer){
+// Function to Generate the Computers Choice
+function getComputerChoice(ComputerChoice){
     let RandNum = Math.random();
     if (RandNum <= .333333){
         return "ROCK";
@@ -19,6 +16,39 @@ function getComputerChoice(computer){
     }
     }
 
+let humanScore = 0;
+let computerScore = 0;
 
+function PlayRound(Human,Computer){
 
- 
+    let HumanChoice = getHumanChoice();
+    let ComputerChoice = getComputerChoice();
+
+    if (HumanChoice == ComputerChoice){
+        return("It's a Tie, Try Again!");
+
+    } if ((HumanChoice == "ROCK") && (ComputerChoice == "SCISSORS")) {
+            return ("You Win! Rock Beats Scissors!");
+    
+    } if ((HumanChoice == "ROCK") && (ComputerChoice == "PAPER")) {
+            return ("You Lose... Paper Beats Rock.");
+
+    } if ((HumanChoice == "PAPER") && (ComputerChoice == "ROCK")) {
+            return ("You Win! Paper Beats Rock!");
+
+    } if ((HumanChoice == "PAPER") && (ComputerChoice == "SCISSORS")) {
+            return ("You Lose... Scissors Beats Paper.");
+
+    } if ((HumanChoice == "SCISSORS") && (ComputerChoice == "PAPER")) {
+            return ("You Win! Scissors Beats Paper!");
+
+    } if ((HumanChoice == "SCISSORS") && (ComputerChoice == "ROCK")) {
+            return ("You Lose... Rock Beats Scissors.");          
+            
+    } else {
+        return ("Someone won")
+    }
+}
+
+console.log(PlayRound());
+
