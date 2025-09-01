aa732c49ab27909f46d4235328c15e78fd26a9f6
Nathan Spain <219461355+nspain96@users.noreply.github.com>
2025-08-31 20:24:43 -0400

    Add CSS

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -36,6 +36,10 @@ const scissorsBtn = document.getElementById('scissors');
 let HumanScore = 0;
 let ComputerScore = 0;
 
+// Create and add a div to display the result of each round
+const div = document.createElement("div");
+document.body.appendChild(div);
+
 // Handle click event for the Rock button
 rockBtn.addEventListener('click', function() {
 
@@ -46,24 +50,24 @@ rockBtn.addEventListener('click', function() {
     if (Result == "Win"){
         HumanScore++;
         if (HumanScore === 5){
-            console.log(`That's Game. Congrats! You Won! Final Score - Human ${HumanScore} vs Computer ${ComputerScore}`);
+            div.textContent = `That's Game. Congrats! You Won! Final Score - Human ${HumanScore} vs Computer ${ComputerScore}`;
             HumanScore = 0;
             ComputerScore = 0;
         } else {
-            console.log(`You ${Result}! ${HumanChoice} beats ${ComputerChoice}. Your Score ${HumanScore} Computer Score ${ComputerScore}`);
+            div.textContent = `You ${Result}! ${HumanChoice} beats ${ComputerChoice}. Your Score ${HumanScore} Computer Score ${ComputerScore}`;
         }
   } else if (Result == "Lose") {
         ComputerScore++;
         if (ComputerScore === 5){
-            console.log(`That's Game. Sorry, You Lost... Final Score - Human ${HumanScore} vs Computer ${ComputerScore}`);
+            div.textContent = `That's Game. Sorry, You Lost... Final Score - Human ${HumanScore} vs Computer ${ComputerScore}`;
             HumanScore = 0;
             ComputerScore = 0;
         } else {
-            console.log(`You ${Result}... ${ComputerChoice} beats ${HumanChoice}. Your Score ${HumanScore} Computer Score ${ComputerScore}`);
+            div.textContent = `You ${Result}... ${ComputerChoice} beats ${HumanChoice}. Your Score ${HumanScore} Computer Score ${ComputerScore}`;
         }
    
   } else {
-    console.log(`Tie. Both of you chose ${HumanChoice}. Your Score ${HumanScore} Computer Score ${ComputerScore}`);
+    div.textContent = `Tie. Both of you chose ${HumanChoice}. Your Score ${HumanScore} Computer Score ${ComputerScore}`;
   }
 
 });
